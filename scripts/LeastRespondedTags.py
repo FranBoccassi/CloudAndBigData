836992d5fd13e31df18887529b0e0f52d56f3e02
Francisco Boccassi <franciscoboccassi@gmail.com>
2021-12-13 17:26:53 +0100

    avances

---
+++ scripts/LeastRespondedTags.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession, functions as F
+spark = SparkSession.builder.appName('Script1').getOrCreate()
+
+path = "/home/mdpbelizon/Tags.csv"
+path2 = "/home/mdpbelizon/miniQ.csv"
+csvDF = spark.read.option("header","true").csv(path)
+csvDF2 = spark.read.option("header","true").csv(path2)
+
+
+totalTags = csvDF.count()
+
+#Preguntas sin responder
+preguntasSinResponder = csvDF2.filter(csvDF2["ClosedDate"] != "NA").select("Id")
+#Tags de las preguntas sin responder
+tagsPSR = csvDF.join(preguntasSinResponder,csvDF["Id"] == preguntasSinResponder["Id"],'left_anti').select("Tag")
+pairs = tagsPSR.withColumn("Count",F.lit(1))
+countedPairs = pairs.groupby("Tag").count().orderBy("Count",ascending = 0).show()
+
+
