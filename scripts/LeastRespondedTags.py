1a0503be46058cc31f0fe953b2d20a1bcd505f06
jorgelasFDI <93203905+jorgelasFDI@users.noreply.github.com>
2021-12-14 13:13:07 +0100

    Update LeastRespondedTags.py

--- scripts/LeastRespondedTags.py
+++ scripts/LeastRespondedTags.py
@@ -1,10 +1,11 @@
 from pyspark.sql import SparkSession, functions as F
+import sys
 spark = SparkSession.builder.appName('Script1').getOrCreate()
 
 path = "/home/mdpbelizon/Tags.csv"
 path2 = "/home/mdpbelizon/miniQ.csv"
-csvDF = spark.read.option("header","true").csv(path)
-csvDF2 = spark.read.option("header","true").csv(path2)
+csvDF = spark.read.option("header","true").csv(sys.argv[1])
+csvDF2 = spark.read.option("header","true").csv(sys.argv[2])
 
 
 totalTags = csvDF.count()
@@ -14,6 +15,6 @@ preguntasSinResponder = csvDF2.filter(csvDF2["ClosedDate"] != "NA").select("Id")
 #Tags de las preguntas sin responder
 tagsPSR = csvDF.join(preguntasSinResponder,csvDF["Id"] == preguntasSinResponder["Id"],'left_anti').select("Tag")
 pairs = tagsPSR.withColumn("Count",F.lit(1))
-countedPairs = pairs.groupby("Tag").count().orderBy("Count",ascending = 0).show()
+countedPairs = pairs.groupby("Tag").count().orderBy("Count",ascending = 0).coalesce(1).write.csv('Test2.csv')
 
 
