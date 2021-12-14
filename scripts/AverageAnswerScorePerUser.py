7ec5a547f551fbb58a792825eda39c9373afb310
Manudpb <80044433+Manudpb@users.noreply.github.com>
2021-12-14 13:11:14 +0100

    Update AverageAnswerScorePerUser.py

--- scripts/AverageAnswerScorePerUser.py
+++ scripts/AverageAnswerScorePerUser.py
@@ -1,8 +1,9 @@
 from pyspark.sql import SparkSession, functions as F
 import csv
+import sys
 spark = SparkSession.builder.appName('Script4').getOrCreate()
 
 
-path = "/home/mdpbelizon/miniA.csv"
-csvDF = spark.read.option("multiLine",True).option("escape", "\"").option("header",True).csv(path)
+csvDF = spark.read.option("multiLine",True).option("escape", "\"").option("header",True).csv(sys.argv[1])
 answerDF = csvDF.select('OwnerUserId','Score').groupBy('OwnerUserId').agg(F.avg('Score')).orderBy('avg(Score)',ascending = 0)
+answerDF.coalesce(1).write.csv("Test3csv")
